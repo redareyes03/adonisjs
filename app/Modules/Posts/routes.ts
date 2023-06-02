cc91682662f4163c201db2db553c107bd5f2f7dc
Ruben Reyes <ruben.burgosr@uanl.edu.mx>
2023-06-02 01:43:31 -0600

    Revert "route handler for posts requests"
    This reverts commit 59a06ad79b972f6313c15feaa68c8f2125b2a5e0.

--- app/Modules/Posts/routes.ts
+++
@@ -1,4 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route';
-
-Route.resource("posts", "PostsController").except(['edit'])
-
--- providers/BaseValidator.ts
+++
@@ -1,8 +0,0 @@
-import {CustomMessages} from "@ioc:Adonis/Core/Validator";
-
-export default class BaseValidator{
-  public messages: CustomMessages = {
-    required: '{{field}} is required to create new post',
-    minLength : 'The {{field}} cannot be empty'
-  }
-}
